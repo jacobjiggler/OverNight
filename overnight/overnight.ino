3af1c47f0a27027c92bb4e347b4d3859ceedd213
matsus2 <satoshi222@hotmail.com>
2015-04-11 16:44:44 -0400

    Initialized pins

--- overnight/overnight.ino
+++ overnight/overnight.ino
@@ -3,14 +3,25 @@
 
 RTC_DS1307 RTC;
 DateTime now; // Holds the current date and time information
+const int hourPin = 3;
+const int minPin = 4;
+const int alarmPin = 5;
+
 boolean alarm = false; // Holds the current state of the alarm
 
 void setup() {
-  // put your setup code here, to run once:
+  //Initialization
   Wire.begin();
-  RTC.begin();
-  Serial.begin(9600); 
+  Serial.begin(9600);
+  
+  //Set up RTC
+  RTC.begin(); 
   RTC.adjust(DateTime(__DATE__, __TIME__));
+  
+  //I/O 
+  pinMode(hourPin, INPUT);
+  pinMode(minPin, INPUT);
+  pinMode(alarmPin, INPUT);
 }
 
 
@@ -19,7 +30,6 @@ void loop() {
   // put your main code here, to run repeatedly:
   showTime();
   delay(2000);
-  
 }
 
 void showTime(){
